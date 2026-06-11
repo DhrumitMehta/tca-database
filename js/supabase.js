2214c33c2035a941854b683661b412c1c7249707
Dhrumit Mehta <dhrumitmehta21@gmail.com>
2026-06-11 13:46:48 +0300

    Update Supabase client config

--- js/supabase.js
+++ js/supabase.js
@@ -2,6 +2,6 @@
 const { createClient } = supabase;  // from the CDN bundle above
 
 const SUPABASE_URL = "https://tlbzeciscxcyiaelsurd.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsYnplY2lzY3hjeWlhZWxzdXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mjc2NTUsImV4cCI6MjA3MzUwMzY1NX0.d7CrsLkLVbRGdOSfbm8Zhr9RqnUZXP5bjJ133EdeHHE";
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsYnplY2lzY3hjeWlhZWxzdXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5Mjc2NTUsImV4cCI6MjA3MzUwMzY1NX0.d7CrsLkLVbRGdOSfbm8Zhr9RqnUZXP5bjJ133EdeHHE";
 
 window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
